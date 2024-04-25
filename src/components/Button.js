ad4564c0ca30be420f56b4181eea1a497c77071a
unknown <gabriel.r.maringolo@gmail.com>
2024-04-25 10:43:18 -0300

    criando componente Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,5 +12,28 @@ const styles = Stylesheet.create({
         borderWidth: 1,
         borderColor: '#888888'
         
+    },
+    operationButton: {
+        color: '#ffffff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 })
+
+export default props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton. push(styles.operationButton)
+}
+
+return(
+    <TouchableHighlight onPress={props.onClick}>
+        <Text style={styleButton}>{props.label}</Text>
+    </TouchableHighlight>
+)
